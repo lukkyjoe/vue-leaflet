452376b56c0500fb166f016f97977681ccb3242a
Joseph Luk <joseph.sc.luk@gmail.com>
2021-12-10 09:57:02 -0800

    Test

--- src/playground/App.vue
+++ src/playground/App.vue
@@ -1,59 +1,60 @@
 <template>
-  <div class="main-wrapper">
-    <div class="menu">
-      <router-link to="/">Home</router-link>
-      <router-link to="/grid-layer">GridLayer</router-link>
-      <router-link to="/tile-layer">TileLayer</router-link>
-      <router-link to="/wms-tile-layer">WmsTileLayer</router-link>
-      <router-link to="/image-overlay">ImageOverlay</router-link>
-      <router-link to="/feature-group">Feature Group</router-link>
-      <router-link to="/circle">Circle</router-link>
-      <router-link to="/circle-marker">CircleMarker</router-link>
-      <router-link to="/control-attribution">Attribution</router-link>
-      <router-link to="/control-custom-message">Custom Message</router-link>
-      <router-link to="/control-layers">Layers</router-link>
-      <router-link to="/control-scale">Scale</router-link>
-      <router-link to="/control-zoom">Zoom</router-link>
-      <router-link to="/geo-json">GeoJSON</router-link>
-      <router-link to="/icon">Icon</router-link>
-      <router-link to="/marker">Marker</router-link>
-      <router-link to="/polygon">Polygon</router-link>
-      <router-link to="/polyline">Polyline</router-link>
-      <router-link to="/popups">Popups</router-link>
-      <router-link to="/rectangle">Rectangle</router-link>
-      <router-link to="/tooltips">Tooltips</router-link>
-    </div>
-    <div class="map-wrapper">
-      <router-view />
-    </div>
-  </div>
+  <map-component :ronde="ronde" class="map"></map-component>
 </template>
+
 <script>
+import MapComponent from "./MapComponent.vue";
+
 export default {
-  components: {},
+  name: "App",
+  components: {
+    MapComponent,
+  },
+  data() {
+    return {
+      ronde: undefined,
+      timeout: undefined,
+    };
+  },
+  mounted() {
+    this.timeout = setTimeout(this.setRonde, 2000);
+  },
+  beforeUnmount() {
+    clearTimeout(this.timeout);
+  },
+  methods: {
+    setRonde() {
+      this.ronde = {
+        geoJson: {
+          type: "FeatureCollection",
+          features: [
+            {
+              type: "Feature",
+              properties: {},
+              geometry: {
+                type: "Polygon",
+                coordinates: [
+                  [
+                    [-12.65625, 34.59704151614417],
+                    [18.6328125, 34.59704151614417],
+                    [18.6328125, 50.958426723359935],
+                    [-12.65625, 50.958426723359935],
+                    [-12.65625, 34.59704151614417],
+                  ],
+                ],
+              },
+            },
+          ],
+        },
+      };
+    },
+  },
 };
 </script>
 
-<style scoped>
-.main-wrapper {
-  display: flex;
-}
-.menu {
-  display: flex;
-  flex-direction: column;
-}
-
-.menu a {
-  padding: 0.25rem 0.5rem;
-  text-decoration: none;
-  font-family: Arial, Helvetica, sans-serif;
-}
-
-.menu .router-link-exact-active {
-  background-color: lightgray;
-}
-.map-wrapper {
-  height: 75vh;
-  width: 100%;
+<style>
+.map {
+  height: 500px;
+  width: 500px;
 }
 </style>
---
+++ src/playground/MapComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="mapContainer">
+    <l-map>
+      <l-geo-json
+        layer-type="overlay"
+        name="Ronde"
+        v-if="ronde"
+        :geojson="ronde.geoJson"
+      />
+      <l-control-layers position="bottomright" />
+    </l-map>
+  </div>
+</template>
+
+<script>
+import LMap from "../components/LMap.vue";
+import LControlLayers from "../components/LControlLayers.vue";
+import LGeoJson from "../components/LGeoJson.vue";
+import "leaflet/dist/leaflet.css";
+
+export default {
+  components: {
+    LMap,
+    LControlLayers,
+    LGeoJson,
+  },
+  props: {
+    ronde: Object,
+  },
+};
+</script>
